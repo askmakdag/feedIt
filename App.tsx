a6482b8d29caeab074c6e87b9ea595132a6278a2
askimakdag <Askmakdag2020>
2022-05-23 15:52:10 +0300

    App Icons and Splash Screens are updated for IOS and Android

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppStack from './src/navigagtion';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   // Authentication flow can be handled here
--- index.js
+++ index.js
@@ -2,15 +2,20 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {NavigationContainer} from '@react-navigation/native';
 import {store} from './src/store';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 function AppRoot() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer style={{flex: 1}}>
